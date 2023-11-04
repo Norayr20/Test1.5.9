b82ef93427ab151f338de711b489d531eab0da95
Norayr <asoyannorayr99@gmail.com>
2023-11-04 12:53:17 +0300

    add commit

---
+++ src/Arreys.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Arreys {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 7};
+        printArray(numbers);
+    }
+
+    public static void printArray(int[] numbers) {
+        System.out.println(Arrays.toString(numbers));
+    }
+}
